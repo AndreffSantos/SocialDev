a578c8fec41796a3175cafab9d37d414ae2adacd
Andre Santos <andrefelipe_santos@outlook.com>
2022-11-17 22:59:47 -0300

    🎉 init(iniciando moibile do projeto):

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>iniciando mobile do projeto com react native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
